9b7244574b5c2dc3c73ca0830d8b966844d1d499
seckcoder <seckcoder@gmail.com>
2015-08-27 15:55:47 -0700

    update install

--- install.sh
+++ install.sh
@@ -5,20 +5,17 @@
 # - one finger tap
 
 
+mkdir -p ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 git clone https://github.com/seckcoder/vim-cpp-mini-config /tmp/vim-cpp-mini-config
 
 cd /tmp/vim-cpp-mini-config
 
-git co -b airbnb origin/airbnb
-
 cp ./bin ~/bin
 cp .bash_profile .bashrc .vimrc ~/
 
-rm -rf ~/.vim
 cp .vim ~/
-
 
 # After config
 # in vim, run `:PluginInstall`
