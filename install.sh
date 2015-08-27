a648e3a564e7aa9c92f7d5136f0346ceb1934c8b
seckcoder <seckcoder@gmail.com>
2015-08-27 16:04:12 -0700

    add install

--- install.sh
+++ install.sh
@@ -7,18 +7,18 @@
 
 rm -rf ~/.vim
 rm -rf ~/bin
+rm -rf /tmp/vim-cpp-mini-config
 
-mkdir -p ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 git clone https://github.com/seckcoder/vim-cpp-mini-config /tmp/vim-cpp-mini-config
 
 cd /tmp/vim-cpp-mini-config
 
-cp ./bin ~/bin
+cp -r ./bin ~/bin
 cp .bash_profile .bashrc .vimrc ~/
 
-cp .vim ~/
+cp -r .vim ~/
 
 # After config
 # in vim, run `:PluginInstall`
